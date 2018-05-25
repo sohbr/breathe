66c39f84ce150b3bf3e7c6f5ce8fbcaa774d1593
sohbr <sohnbrian@gmail.com>
2018-05-24 23:48:12 -0700

    animation motion

--- src/Bubble.js
+++ src/Bubble.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 
-let notchSize = 9;
+let notchSize = 3.5;
 
-let x1 = Math.sin(notchSize * Math.PI/180) * 130;
-let y1 = Math.sqrt(Math.pow(130,2) - Math.pow(x1,2));
-let x2 = 250 + x1;
-let y2 = 250 - y1;
+let x1temp = Math.sin(notchSize * Math.PI/180) * 130;
+let y1temp = Math.sqrt(Math.pow(130,2) - Math.pow(x1temp,2));
+let x1 = 250 - x1temp;
+let y1 = 250 - y1temp;
+let x2 = 250 + x1temp;
+let y2 = 250 - y1temp;
 
-let x3 = Math.sin((30 + notchSize) * Math.PI/180) * 130;
-let y3 = Math.sqrt(Math.pow(130,2) - Math.pow(x3,2));
-let x4 = 250 + x3;
-let y4 = 250 + y3;
+let x3temp = Math.sin((30 + notchSize) * Math.PI/180) * 130;
+let y3temp = Math.sqrt(Math.pow(130,2) - Math.pow(x3temp,2));
+let x3 = 250 + x3temp;
+let y3 = 250 + y3temp;
 
-let x5 = Math.sin((30 - notchSize) * Math.PI/180) * 130;
-let y5 = Math.sqrt(Math.pow(130,2) - Math.pow(x5,2));
-let x6 = 250 + x5;
-let y6 = 250 + y5;
+let x4temp = Math.sin((30 - notchSize) * Math.PI/180) * 130;
+let y4temp = Math.sqrt(Math.pow(130,2) - Math.pow(x4temp,2));
+let x4 = 250 + x4temp;
+let y4 = 250 + y4temp;
 
-let x7 = 250 - x5;
-let y7 = 250 + y5;
+let x5 = 250 - x4temp;
+let y5 = 250 + y4temp;
 
-let x8 = 250 - x3;
-let y8 = 250 + y3;
+let x6 = 250 - x3temp;
+let y6 = 250 + y3temp;
 
-let x9 = 250 - x1;
-let y9 = 250 - y1;
 
 
 class Bubble extends Component {
@@ -32,18 +32,31 @@ class Bubble extends Component {
   render () {
     return (
       <svg height="500" width="500">
-        <path d={`M250,250 m${-x1},${-y1} A130,130 0 0,1 ${x2},${y2}`}
-          fill="none" stroke="rgb(237, 237, 237)" strokeWidth="4" />
-        <path d={`M${x2},${y2} A130,130 0 0,1 ${x4},${y4}`}
-          fill="none" stroke="rgb(107, 177, 224)" strokeWidth="4" />
-        <path d={`M${x4},${y4} A130,130 0 0,1 ${x6},${y6}`}
-          fill="none" stroke="rgb(237, 237, 237)" strokeWidth="4" />
-        <path d={`M${x6},${y6} A130,130 0 0,1 ${x7},${y7}`}
-          fill="none" stroke="rgb(204, 204, 204)" strokeWidth="4" />
-        <path d={`M${x7},${y7} A130,130 0 0,1 ${x8},${y8}`}
-          fill="none" stroke="rgb(237, 237, 237)" strokeWidth="4" />
-        <path d={`M${x8},${y8} A130,130 0 0,1 ${x9},${y9}`}
-          fill="none" stroke="rgb(188, 107, 224)" strokeWidth="4" />
+        <path id="circlePath" d={`M${x1},${y1} A130,130 0 0,1 ${x2},${y2}`}
+          fill="none" stroke="rgb(237, 237, 237)" strokeWidth="3" />
+        <path id="circlePath" d={`M${x2},${y2} A130,130 0 0,1 ${x3},${y3}`}
+          fill="none" stroke="rgb(107, 177, 224)" strokeWidth="3" />
+        <path id="circlePath" d={`M${x3},${y3} A130,130 0 0,1 ${x4},${y4}`}
+          fill="none" stroke="rgb(237, 237, 237)" strokeWidth="3" />
+        <path id="circlePath" d={`M${x4},${y4} A130,130 0 0,1 ${x5},${y5}`}
+          fill="none" stroke="rgb(204, 204, 204)" strokeWidth="3" />
+        <path id="circlePath" d={`M${x5},${y5} A130,130 0 0,1 ${x6},${y6}`}
+          fill="none" stroke="rgb(237, 237, 237)" strokeWidth="3" />
+        <path id="circlePath" d={`M${x6},${y6} A130,130 0 0,1 ${x1},${y1}`}
+          fill="none" stroke="rgb(188, 107, 224)" strokeWidth="3" />
+
+        <path d={`M${x1},${y1} A1,1, 0 0,0 ${x2},${y2}`}
+          fill="rgb(237,237,237)" stroke="rgb(237,237,237)" strokeWidth="0.1" />
+        <path d={`M${x3},${y3} A1,1, 0 0,0 ${x4},${y4}`}
+          fill="rgb(237,237,237)" stroke="rgb(237,237,237)" strokeWidth="0.1" />
+        <path d={`M${x5},${y5} A1,1, 0 0,0 ${x6},${y6}`}
+          fill="rgb(237,237,237)" stroke="rgb(237,237,237)" strokeWidth="0.1" />
+
+        <circle cx="250" cy="250" r="115" stroke="rgb(182, 169, 242)" fill="rgb(182, 169, 242)">
+          <animateMotion dur="10s" repeatCount="indefinite" rotate="auto">
+          </animateMotion>
+        </circle>
+
       </svg>
 
     );
