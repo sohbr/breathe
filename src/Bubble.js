0f3b8bad5eedcc7fcbff6706f67893277d6a6fd9
sohbr <sohnbrian@gmail.com>
2018-06-05 16:49:14 -0700

    completed orbit animation

--- src/Bubble.js
+++ src/Bubble.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Bubble.css';
 
-let notchSize = 3.5;
+let notchSize = 3;
 
 let x1temp = Math.sin(notchSize * Math.PI/180) * 130;
 let y1temp = Math.sqrt(Math.pow(130,2) - Math.pow(x1temp,2));
@@ -33,35 +33,35 @@ class Bubble extends Component {
   render () {
     return (
       <svg height="500" width="500">
-        <path d={`M${x1},${y1} A130,130 0 0,1 ${x2},${y2}`}
+        <path className="grow" d={`M${x1},${y1} A130,130 0 0,1 ${x2},${y2}`}
           fill="none" stroke="rgb(237, 237, 237)" strokeWidth="3" />
-        <path d={`M${x2},${y2} A130,130 0 0,1 ${x3},${y3}`}
+        <path className="grow" d={`M${x2},${y2} A130,130 0 0,1 ${x3},${y3}`}
           fill="none" stroke="rgb(107, 177, 224)" strokeWidth="3" />
-        <path d={`M${x3},${y3} A130,130 0 0,1 ${x4},${y4}`}
+        <path className="grow" d={`M${x3},${y3} A130,130 0 0,1 ${x4},${y4}`}
           fill="none" stroke="rgb(237, 237, 237)" strokeWidth="3" />
-        <path d={`M${x4},${y4} A130,130 0 0,1 ${x5},${y5}`}
+        <path className="grow" d={`M${x4},${y4} A130,130 0 0,1 ${x5},${y5}`}
           fill="none" stroke="rgb(204, 204, 204)" strokeWidth="3" />
-        <path d={`M${x5},${y5} A130,130 0 0,1 ${x6},${y6}`}
+        <path className="grow" d={`M${x5},${y5} A130,130 0 0,1 ${x6},${y6}`}
           fill="none" stroke="rgb(237, 237, 237)" strokeWidth="3" />
-        <path d={`M${x6},${y6} A130,130 0 0,1 ${x1},${y1}`}
+        <path className="grow" d={`M${x6},${y6} A130,130 0 0,1 ${x1},${y1}`}
           fill="none" stroke="rgb(188, 107, 224)" strokeWidth="3" />
 
-        <path d={`M${x1},${y1} A1,1, 0 0,0 ${x2},${y2}`}
+        <path className="grow" d={`M${x1},${y1} A1,1, 0 0,0 ${x2},${y2}`}
           fill="rgb(237,237,237)" stroke="rgb(237,237,237)" strokeWidth="0.1" />
-        <path d={`M${x3},${y3} A1,1, 0 0,0 ${x4},${y4}`}
+        <path className="grow" d={`M${x3},${y3} A1,1, 0 0,0 ${x4},${y4}`}
           fill="rgb(237,237,237)" stroke="rgb(237,237,237)" strokeWidth="0.1" />
-        <path d={`M${x5},${y5} A1,1, 0 0,0 ${x6},${y6}`}
+        <path className="grow" d={`M${x5},${y5} A1,1, 0 0,0 ${x6},${y6}`}
           fill="rgb(237,237,237)" stroke="rgb(237,237,237)" strokeWidth="0.1" />
 
-        <path id="circle" d={`M${x1},${y1} A1,1, 0 0,0 ${x2},${y2}`}
+        <path className="orbit"  d={`M${x1},${y1} A1,1, 0 0,0 ${x2},${y2}`}
           fill="red" stroke="rgb(237,237,237)" strokeWidth="0.1" />
+        <path className="orbit" d={`M${x1},${y1} A130,130 0 0,1 ${x2},${y2}`}
+          fill="none" stroke="rgb(255,0,0)" strokeWidth="3" />
 
         <div></div>
 
-        <circle cx="250" cy="250" r="115" stroke="rgb(182, 169, 242)"
+        <circle className="grow" cx="250" cy="250" r="115" stroke="rgb(182, 169, 242)"
           fill="rgb(182, 169, 242)">
-          <animateMotion dur="10s" repeatCount="indefinite" rotate="auto">
-          </animateMotion>
         </circle>
 
       </svg>
